b07c3222116dfdbffea230b72b1a7dbce3030ba4
Vincent Dupont <vidupont@gmail.com>
2017-08-01 16:39:11 +0200

    updated

--- startup/start.sh
+++ startup/start.sh
@@ -1,3 +1,12 @@
+# @Author: Vincent Dupont <vincent>
+# @Date:   2017-06-09T19:18:51+02:00
+# @Email:  vidupont@gmail.com
+# @Filename: start.sh
+# @Last modified by:   vincent
+# @Last modified time: 2017-08-01T16:23:13+02:00
+
+
+
 #!/bin/bash
 #
 # Dupont Vincent - 11/06/2017
@@ -15,7 +24,7 @@ lsusb
 #amixer
 
 # Turn off the Screen Saver - Temporary workaround
-xset s off
+# xset s off
 
 # Start the interface application
  # Proximus Animation Logo
