792efce54c1c23fdff66e2969b168c1003096711
Vincent Dupont <vidupont@gmail.com>
2017-08-02 16:10:36 +0200

    PlatformIO

--- services/platformio.sh
+++ services/platformio.sh
@@ -1,8 +1,14 @@
-# Initialise the platformio agent, do it once the ENV is set
+# @Author: Vincent Dupont <vincent>
+# @Date:   2017-06-09T19:18:51+02:00
+# @Email:  vidupont@gmail.com
+# @Filename: platformio.sh
+# @Last modified by:   vincent
+# @Last modified time: 2017-08-02T16:10:30+02:00
 
+# Initialise the platformio agent, do it once the ENV is set
 # Starts the PlatformIO agent if enabled
 
-if [ "$PLATFORMIO_AGENT"="1" ]; then
+if [ "$PLATFORMIO_AGENT" == "1" ]; then
   echo "Starting the platfform IO agent."
-  pio remote agent start -d $KIOSK_DATA &
+  pio remote agent start -d /SERVICES &
 fi
