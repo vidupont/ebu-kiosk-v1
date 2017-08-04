3258a62274db0f4da0a3648479dd2de93682663c
Vincent Dupont <vidupont@gmail.com>
2017-08-04 23:25:45 +0200

    Update NodeRed

--- services/node-red/app/start.sh
+++ services/node-red/app/start.sh
@@ -1,10 +1,19 @@
+# @Author: Vincent Dupont <vincent>
+# @Date:   2017-08-03T10:08:36+02:00
+# @Email:  vidupont@gmail.com
+# @Filename: start.sh
+# @Last modified by:   vincent
+# @Last modified time: 2017-08-04T23:24:44+02:00
+
+
+
 #!/bin/bash
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # Make the default flows available in the user library
 mkdir -p /data/node-red/user/lib/flows || true
-cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
+cp /SERVICES/node-red/app/flows/* /data/node-red/user/lib/flows/
 
 # Start app
-node-red --settings /usr/src/app/settings.js
+node-red --settings /SERVICES/node-red/app/settings.js
