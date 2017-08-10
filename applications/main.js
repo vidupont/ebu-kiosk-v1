778c4eeea2dcb3150ef9bc9150da02f53e82ea67
Vincent Dupont <vidupont@gmail.com>
2017-08-10 12:12:49 +0200

    Node-Red

--- applications/main.js
+++ applications/main.js
@@ -1,41 +1,51 @@
-const electron = require('electron');
+/**
+ * @Author: Vincent Dupont <vincent>
+ * @Date:   2017-08-04T11:52:12+02:00
+ * @Email:  vidupont@gmail.com
+ * @Filename: main.js
+ * @Last modified by:   vincent
+ * @Last modified time: 2017-08-10T11:53:28+02:00
+ */
+
 const path = require('path');
 
 const { app, BrowserWindow } = electron;
 
 // simple parameters initialization
 const electronConfig = {
-  URL_LAUNCHER_TOUCH: process.env.URL_LAUNCHER_TOUCH === '1' ? 1 : 0,
-  URL_LAUNCHER_TOUCH_SIMULATE: process.env.URL_LAUNCHER_TOUCH_SIMULATE === '1' ? 1 : 0,
-  URL_LAUNCHER_FRAME: process.env.URL_LAUNCHER_FRAME === '1' ? 1 : 0,
-  URL_LAUNCHER_KIOSK: process.env.URL_LAUNCHER_KIOSK === '1' ? 1 : 0,
-  URL_LAUNCHER_NODE: process.env.URL_LAUNCHER_NODE === '1' ? 1 : 0,
-  URL_LAUNCHER_WIDTH: parseInt(process.env.URL_LAUNCHER_WIDTH || 1920, 10),
-  URL_LAUNCHER_HEIGHT: parseInt(process.env.URL_LAUNCHER_HEIGHT || 1080, 10),
-  URL_LAUNCHER_TITLE: process.env.URL_LAUNCHER_TITLE || 'RESIN.IO',
-  URL_LAUNCHER_CONSOLE: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
-  URL_LAUNCHER_URL: process.env.URL_LAUNCHER_URL || `file:///${path.join(__dirname, 'data', 'index.html')}`,
-  URL_LAUNCHER_ZOOM: parseFloat(process.env.URL_LAUNCHER_ZOOM || 1.0),
-  URL_LAUNCHER_OVERLAY_SCROLLBARS: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
+  KIOSK_TOUCH: process.env.KIOSK_TOUCH === '1' ? 1 : 0,
+  KIOSK_TOUCH_SIMULATE: process.env.KIOSK_TOUCH_SIMULATE === '1' ? 1 : 0,
+  KIOSK_FRAME: process.env.KIOSK_FRAME === '1' ? 1 : 0,
+  KIOSK_KIOSK: process.env.KIOSK_KIOSK === '1' ? 1 : 0,
+  KIOSK_NODE: process.env.KIOSK_NODE === '1' ? 1 : 0,
+  KIOSK_X: parseInt(process.env.KIOSK_X || 0, 10),
+  KIOSK_Y: parseInt(process.env.KIOSK_Y || 0, 10),
+  KIOSK_WIDTH: parseInt(process.env.KIOSK_WIDTH || 1920, 10),
+  KIOSK_HEIGHT: parseInt(process.env.KIOSK_HEIGHT || 1080, 10),
+  KIOSK_TITLE: process.env.KIOSK_TITLE || 'RESIN.IO',
+  KIOSK_CONSOLE: process.env.KIOSK_CONSOLE === '1' ? 1 : 0,
+  KIOSK_URL: process.env.KIOSK_URL || `file:///${path.join(__dirname, 'data', 'index.html')}`,
+  KIOSK_ZOOM: parseFloat(process.env.KIOSK_ZOOM || 1.0),
+  KIOSK_OVERLAY_SCROLLBARS: process.env.KIOSK_CONSOLE === '1' ? 1 : 0,
 };
 
 // enable touch events if your device supports them
-if (electronConfig.URL_LAUNCHER_TOUCH) {
+if (electronConfig.KIOSK_TOUCH) {
   app.commandLine.appendSwitch('--touch-devices');
 }
 // simulate touch events - might be useful for touchscreen with partial driver support
-if (electronConfig.URL_LAUNCHER_TOUCH_SIMULATE) {
+if (electronConfig.KIOSK_TOUCH_SIMULATE) {
   app.commandLine.appendSwitch('--simulate-touch-screen-with-mouse');
 }
 
 if (process.env.NODE_ENV === 'development') {
   console.log('Running in development mode');
   Object.assign(electronConfig, {
-    URL_LAUNCHER_HEIGHT: 600,
-    URL_LAUNCHER_WIDTH: 800,
-    URL_LAUNCHER_KIOSK: 0,
-    URL_LAUNCHER_CONSOLE: 1,
-    URL_LAUNCHER_FRAME: 1,
+    KIOSK_HEIGHT: 600,
+    KIOSK_WIDTH: 800,
+    KIOSK_KIOSK: 0,
+    KIOSK_CONSOLE: 1,
+    KIOSK_FRAME: 1,
   });
 }
 
@@ -45,15 +55,17 @@ if (process.env.NODE_ENV === 'development') {
 app.on('ready', () => {
   // here we actually configure the behavour of electronJS
   const window = new BrowserWindow({
-    width: electronConfig.URL_LAUNCHER_WIDTH,
-    height: electronConfig.URL_LAUNCHER_HEIGHT,
-    frame: !!(electronConfig.URL_LAUNCHER_FRAME),
-    title: electronConfig.URL_LAUNCHER_TITLE,
-    kiosk: !!(electronConfig.URL_LAUNCHER_KIOSK),
+    x:electronConfig.KIOSK_X,
+    y:electronConfig.KIOSK_Y,
+    width: electronConfig.KIOSK_WIDTH,
+    height: electronConfig.KIOSK_HEIGHT,
+    frame: !!(electronConfig.KIOSK_FRAME),
+    title: electronConfig.KIOSK_TITLE,
+    kiosk: !!(electronConfig.KIOSK_KIOSK),
     webPreferences: {
-      nodeIntegration: !!(electronConfig.URL_LAUNCHER_NODE),
-      zoomFactor: electronConfig.URL_LAUNCHER_ZOOM,
-      overlayScrollbars: !!(electronConfig.URL_LAUNCHER_OVERLAY_SCROLLBARS),
+      nodeIntegration: !!(electronConfig.KIOSK_NODE),
+      zoomFactor: electronConfig.KIOSK_ZOOM,
+      overlayScrollbars: !!(electronConfig.KIOSK_OVERLAY_SCROLLBARS),
     },
   });
 
@@ -65,10 +77,10 @@ app.on('ready', () => {
 
   // if the env-var is set to true,
   // a portion of the screen will be dedicated to the chrome-dev-tools
-  if (electronConfig.URL_LAUNCHER_CONSOLE) {
+  if (electronConfig.KIOSK_CONSOLE) {
     window.openDevTools();
   }
 
   // the big red button, here we go
-  window.loadURL(electronConfig.URL_LAUNCHER_URL);
+  window.loadURL(electronConfig.KIOSK_URL);
 });
