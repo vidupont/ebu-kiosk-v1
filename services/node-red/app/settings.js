afa3577818d2e16702c7c8e9c78d00c8b1ac6146
Vincent Dupont <vidupont@gmail.com>
2017-08-10 13:20:14 +0200

    fix node-red IP

--- services/node-red/app/settings.js
+++ services/node-red/app/settings.js
@@ -4,10 +4,8 @@
  * @Email:  vidupont@gmail.com
  * @Filename: settings.js
  * @Last modified by:   vincent
- * @Last modified time: 2017-08-10T12:20:13+02:00
+ * @Last modified time: 2017-08-10T13:19:47+02:00
  */
-
-
 
 /**
  * Copyright 2013, 2016 IBM Corp.
@@ -27,7 +25,7 @@
 
 // The `https` setting requires the `fs` module. Uncomment the following
 // to make it available:
-//var fs = require("fs");
+var fs = require("fs");
 
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
@@ -36,7 +34,7 @@ module.exports = {
     // By default, the Node-RED UI accepts connections on all IPv4 interfaces.
     // The following property can be used to listen on a specific interface. For
     // example, the following would only allow connections from the local machine.
-    uiHost: process.env.RED_UIHOST || "127.0.0.1",
+    //uiHost: "127.0.0.1",
 
     // Retry time in milliseconds for MQTT connections
     mqttReconnectTime: 15000,
@@ -63,7 +61,7 @@ module.exports = {
 
     // To enabled pretty-printing of the flow within the flow file, set the following
     //  property to true:
-    //flowFilePretty: true,
+    flowFilePretty: true,
 
     // By default, credentials are encrypted in storage using a generated key. To
     // specify your own secret, set the following property.
@@ -84,7 +82,7 @@ module.exports = {
     // By default, the Node-RED UI is available at http://localhost:1880/
     // The following property can be used to specifiy a different root path.
     // If set to false, this is disabled.
-    //httpAdminRoot: '/admin',
+    httpAdminRoot: '/admin',
 
     // Some nodes, such as HTTP In, can be used to listen for incoming http requests.
     // By default, these are served relative to '/'. The following property
@@ -107,7 +105,7 @@ module.exports = {
 
     // If you installed the optional node-red-dashboard you can set it's path
     // relative to httpRoot
-    //ui: { path: "ui" },
+    ui: { path: "ui" },
 
     // Securing Node-RED
     // -----------------
